0cf6ec11d534788da416119dc0a4ca6d08e05b13
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-07-24 14:58:42 +0200

    rm File access here !

--- EasyExample/components/AppIso/AppMemAccess.cpp
+++ EasyExample/components/AppIso/AppMemAccess.cpp
@@ -9,14 +9,13 @@
 #include <iostream>
 #include <sstream>
 #include <string.h>
-#include "settings.h"
+#include "Settings/settings.h"
 #include "AppMemAccess.h"
 
 #if defined(ESP_PLATFORM)
 #include <sys/param.h>
 #include "esp_err.h"
 #include "esp_log.h"
-#include "esp_spiffs.h"
 
 const char *D_D_D_D_D_D_D_I64_X = "%d=%d,%d,%d,%d,%d,%d,%I64X";
 const char *D_D_D_D_D_D_D_L_X 	= "%d=%d,%d,%d,%d,%d,%d,%lX";
@@ -57,92 +56,8 @@ static bool getValue(const VT_AUXAPP_T& auxEntry, char* value, size_t size);
 
 extern const int FIRST_AUX;
 extern const int  LAST_AUX;
-
-/* ****************   Object pool access   *********************************** */
-
-
-#if defined(ESP_PLATFORM)
-static esp_err_t register_vfs()
-{
-    ESP_LOGI(TAG, "Initializing SPIFFS");
-
-    esp_vfs_spiffs_conf_t conf = {
-        .base_path = "/spiffs",
-        .partition_label = NULL,
-        .max_files = 5,
-        .format_if_mount_failed = false
-    };
-
-    // Use settings defined above to initialize and mount SPIFFS filesystem.
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-        }
-    }
-
-    size_t total = 0, used = 0;
-    ret = esp_spiffs_info(conf.partition_label, &total, &used);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
-    } else {
-        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-    }
-
-    return ret;
-}
-#endif // def ESP_PLATFORM
-
-
-iso_u32 LoadPoolFromFile(const char * pcFilename, iso_u8 ** pPoolBuff)
-{
-
-	   /* load pool from file into RAM */
-	   FILE *pFile;
-	   iso_u32 u32PoolSize = 0;
-
-
-#if defined(ESP_PLATFORM)
-    if ( register_vfs() == ESP_OK ) {
-#endif // def ESP_PLATFORM
-
-	   if (*pPoolBuff)
-	   {   /* free the pool data RAM */
-		  free(*pPoolBuff);
-		  *pPoolBuff = 0;
-	   }
-
-	   ESP_LOGI(TAG, "Opening file");
-	   pFile = fopen(pcFilename, "rb");
-	   if (pFile == NULL) {
-	      ESP_LOGE(TAG, "Failed to open file for reading");
-	   }
-	   if (pFile)
-	   {
-		  fseek(pFile, 0L, SEEK_END);
-		  u32PoolSize = static_cast<iso_u32>(ftell(pFile));
-		  *pPoolBuff = reinterpret_cast<iso_u8*>(malloc(u32PoolSize));
-		  fseek(pFile, 0L, SEEK_SET);
-		  u32PoolSize = (iso_u32)fread(*pPoolBuff, sizeof(iso_u8), u32PoolSize, pFile);
-		  fclose(pFile);
-	   }
-
-#if defined(ESP_PLATFORM)
-       ESP_LOGI(TAG, "Pool size: %d, file: %s", u32PoolSize, pcFilename);
-    }
-#endif // def ESP_PLATFORM
-
-   return u32PoolSize;
-}
-
-
-
 /* ****************   Auxiliary Assignments  *********************************** */
+
 iso_s16 getAuxAssignment(const char auxSection[], VT_AUXAPP_T asAuxAss[])
 {
 
@@ -179,8 +94,7 @@ iso_s16 getAuxAssignment(const char auxSection[], VT_AUXAPP_T asAuxAss[])
    return idxAux;
 }
 
-
-bool parseAuxEntry(char* entry, VT_AUXAPP_T* auxEntry)
+static bool parseAuxEntry(char* entry, VT_AUXAPP_T* auxEntry)
 {
     int wObjID_Fun;
     int wObjID_Input;
@@ -232,7 +146,7 @@ void setAuxAssignment(const char section[], VT_AUXAPP_T asAuxAss[], iso_s16 iNum
    char buffer[64];
 
    // write aux entries
-   for (iso_s16 idx = 0; idx < iNumberOfAssigns; idx++)
+   for (int8_t idx = 0; idx < iNumberOfAssigns; idx++)
    {
       VT_AUXAPP_T* auxEntry = &asAuxAss[idx];
       getKeyByID((iso_s16)auxEntry->wObjID_Fun, key, sizeof(key));
@@ -246,7 +160,7 @@ void setAuxAssignment(const char section[], VT_AUXAPP_T asAuxAss[], iso_s16 iNum
 	  sprintf_s(buffer, sizeof(buffer), D_D_D_D_D_D_D_I64_X,
 #endif //!defined(USE_L_FOR_64BIT)
       auxEntry->wObjID_Fun, auxEntry->wObjID_Input, auxEntry->eAuxType, auxEntry->wManuCode, auxEntry->wModelIdentCode,
-      auxEntry->qPrefAssign, auxEntry->bFuncAttribute, name);
+         auxEntry->qPrefAssign, auxEntry->bFuncAttribute, name);
       setString(section, key, buffer);
    }
 }
@@ -296,7 +210,7 @@ static bool getValue(const VT_AUXAPP_T& auxEntry, char* value, size_t size)
 	sprintf_s(value, size, D_D_D_D_D_D_D_I64_X,
 #endif // !defined(USE_L_FOR_64BIT)
         auxEntry.wObjID_Fun, auxEntry.wObjID_Input, auxEntry.eAuxType, auxEntry.wManuCode, auxEntry.wModelIdentCode,
-        auxEntry.qPrefAssign, auxEntry.bFuncAttribute, name);
+    auxEntry.qPrefAssign, auxEntry.bFuncAttribute, name);
     return true;
 }
 
